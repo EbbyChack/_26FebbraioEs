7d6ccfcc9a83cbb5e72b23fd0ef85f3d9842a91a
Ebby Chakkungal <121340114+EbbyChack@users.noreply.github.com>
2024-02-26 16:12:40 +0100

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _26FebbraioEs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Dipendente(Models.Dipendente dipendente)
+        {
+
+            List<Models.Dipendente> ListaDipendenti = new List<Models.Dipendente>();  
+
+            ViewBag.Nome = dipendente.Nome;
+            ViewBag.Cognome = dipendente.Cognome;
+            ViewBag.Indirizzo = dipendente.Indirizzo;
+            ViewBag.CodiceFiscale = dipendente.CodiceFiscale;
+            ViewBag.Coniugato = dipendente.Coniugato;
+            ViewBag.FigliACarico = dipendente.FigliACarico;
+            ViewBag.Mansione = dipendente.Mansione;
+
+            ListaDipendenti.Add(dipendente);
+
+            return View();
+        }
+    }
+}
---
+++ Models/Pagamento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _26FebbraioEs.Models
+{
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public int IdDipendente { get; set; }
+        public DateTime PeriodoPagamento { get; set; }
+        public decimal Ammontare {  get; set; }
+        public string TipoPagamento { get; set; }
+
+    }
+}
